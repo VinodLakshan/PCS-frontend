e48a1ff7a01d7a000aaa773a68989c15a8b4b3a3
vinodlakshan <vinodlakshan16@gmail.com>
2021-11-22 12:42:17 +0530

    login and registration completed

--- Assets/Custom/Script/Login.js
+++ Assets/Custom/Script/Login.js
@@ -1,4 +1,22 @@
+baseUrl = "http://localhost:8080/pcs";
+
 $('#btnLogin').click(function (){
     var formdata = $('#formLogin').serializeObject();
-    console.log(formdata);
+    $.ajax({
+        type: "POST",
+        url:  baseUrl + "/employee/login",
+        data: JSON.stringify(formdata),
+        contentType: "application/json",
+        success: function (response) {
+            sessionStorage.setItem("employee", JSON.stringify(response.data));
+            sessionStorage.setItem("token", response.token);
+            window.location.href = "Dashboard.html";
+        },
+        error: function (response) {
+            if (response.status == 403){
+                $('#alertError').text(response.responseJSON.message)
+                $('#alertError').removeClass("d-none");
+            }
+        }
+    });
 });
